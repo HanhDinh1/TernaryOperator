c977d277f9d2dc4d21ab79f3c1e4ff2c1b5e41a9
HanhDinh1 <ms.myhanh89@gmail.com>
2023-05-02 07:17:07 -0400

    Done

---
+++ TernaryOperator/src/com/hanh/ternaryoperator/TernaryOperator.java
@@ -0,0 +1,15 @@
+package com.hanh.ternaryoperator;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int a = 13;
+		int b = 12;
+		
+		int max = a > b ? a : b;
+		
+		System.err.println("max is " + max);
+
+	}
+
+}
